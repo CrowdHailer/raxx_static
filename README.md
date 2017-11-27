3d2f08a474d872187b87816a83f6357a080cae47
CrowdHailer <peterhsaxton@gmail.com>
2017-11-27 21:12:08 +0000

    extracted from tokumei project

---
+++ README.md
@@ -0,0 +1,27 @@
+# Raxx.Static
+
+**[Raxx](https://github.com/crowdhailer/raxx) middleware for serving static content.**
+
+[![Hex pm](http://img.shields.io/hexpm/v/raxx_static.svg?style=flat)](https://hex.pm/packages/raxx_static)
+[![Build Status](https://secure.travis-ci.org/CrowdHailer/raxx_static.svg?branch=master
+"Build Status")](https://travis-ci.org/CrowdHailer/raxx_static)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](LICENSE)
+
+- [Install from hex.pm](https://hex.pm/packages/raxx_static)
+- [Documentation available on hexdoc](https://hexdocs.pm/raxx_static)
+- [Raxx discussion on slack](https://elixir-lang.slack.com/messages/C56H3TBH8/)
+
+## Getting Started
+
+Add routes for static assets to any server module.
+
+```elixir
+defmodule MyApp.WWW do
+  use Raxx.Server
+  use Raxx.Static, "./public"
+
+  @impl Raxx.Server
+  def handle_request(request, state) do
+  # ...
+end
+```
---
+++ mix.exs
@@ -0,0 +1,47 @@
+defmodule RaxxStatic.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :raxx_static,
+      version: "0.5.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_options: [
+        warnings_as_errors: true
+      ],
+      description: description(),
+      docs: [extras: ["README.md"], main: "readme"],
+      package: package()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:raxx, "~> 0.14.2"},
+      {:mime, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Raxx middleware for serving static content.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Peter Saxton"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/crowdhailer/raxx_static"}
+    ]
+  end
+end
